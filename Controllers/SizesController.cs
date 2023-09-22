460e1d2f482ca35bf47d8e7c172c4ba3e1e0efa2
Dayo Oladapo <dayoesq@live.com>
2023-09-22 18:36:21 +0300

    Refactor: Implemented UoW in the controllers

---
+++ Controllers/SizesController.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using Market.Models;
+using Market.Models.DTOS.Sizes;
+using Market.Repositories;
+using Market.Repositories.UnitOfWork;
+using Market.Utils;
+using Market.Utils.Constants;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = Operation.SuperAdmin)]
+[ApiController]
+[Route("[controller]")]
+public class SizesController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IRepository<Size, int> _sizeRepository;
+    private readonly IMapper _mapper;
+
+    public SizesController(IMapper mapper, IUnitOfWork unitOfWork, IRepository<Size, int> sizeRepository)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+        _sizeRepository = sizeRepository;
+    }
+
+    [AllowAnonymous]
+    [HttpGet(Name = "get-sizes")]
+    public async Task<IActionResult> GetSizes()
+    {
+        var sizes = await _sizeRepository.GetAllAsync();
+        var result = _mapper.Map<List<SizeDto>>(sizes);
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id}", Name = "get-size")]
+    public async Task<IActionResult> GetSize(int id)
+    {
+        var size = await _sizeRepository.GetAsync(id);
+        if (size == null) return NotFound(new { message = Errors.NotFound404 });
+        var result = _mapper.Map<SizeDto>(size);
+        return Ok(result);
+
+    }
+
+    [HttpDelete("{id}", Name = "delete-size")]
+    public async Task<IActionResult> DeleteSize(int id)
+    {
+        var size = await _sizeRepository.DeleteAsync(id);
+        if (!size) return NotFound(new { message = Errors.NotFound404 });
+        await _unitOfWork.CommitAsync();
+        return Ok(new { message = ResponseMessage.Success });
+
+    }
+}
